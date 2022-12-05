32e5aa10b3facd52106ac0a27e96b7e8e237f022
kelly <kayzspace29@gmail.com>
2022-12-05 08:43:37 -0800

    correction

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,14 +1,6 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list) - 1:
         return 'None'
     else:
         return my_list[idx]
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-
