d537b6deaaa9a3dbaf8a01c1962c2bc025c167cd
kelly <kayzspace29@gmail.com>
2023-01-10 14:56:08 -0800

    double check

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     except FileNotFoundError:
         items = []
     items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+   (save_to_json_file(items, "add_item.json"))
